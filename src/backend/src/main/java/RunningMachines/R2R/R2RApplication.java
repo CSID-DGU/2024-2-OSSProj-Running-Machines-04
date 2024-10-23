bfed8620a30023ab02d5ea2ab3ac26a0c6fc3ab6
jjin70 <jjin0434@naver.com>
2024-10-24 01:19:47 +0900

    BE: [feat] 스프링 초기 세팅

---
+++ src/backend/src/main/java/RunningMachines/R2R/R2RApplication.java
@@ -0,0 +1,13 @@
+package RunningMachines.R2R;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2RApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2RApplication.class, args);
+	}
+
+}
