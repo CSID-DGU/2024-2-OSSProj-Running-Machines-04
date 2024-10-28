3a47580c02494820fe87d07810cc32d598137a25
jjin70 <jjin0434@naver.com>
2024-10-28 18:50:13 +0900

    BE: [feat] swagger Authorize 추가 #4

--- src/backend/src/main/java/RunningMachines/R2R/global/config/SwaggerConfig.java
+++ src/backend/src/main/java/RunningMachines/R2R/global/config/SwaggerConfig.java
@@ -3,16 +3,22 @@ package RunningMachines.R2R.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SCHEME_NAME = "Authorization";
+    private static final String SCHEME_TYPE = "Bearer";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes(SCHEME_NAME, securityScheme()))
+                .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME))
                 .info(getInfo());
     }
 
@@ -21,5 +27,14 @@ public class SwaggerConfig {
                 .title("R2R API Document")
                 .description("R2R document 입니다.")
                 .version("1.0.0");
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .name(SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(SCHEME_TYPE)
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
     }
 }
