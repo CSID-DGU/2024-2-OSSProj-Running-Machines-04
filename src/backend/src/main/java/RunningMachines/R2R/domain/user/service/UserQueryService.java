2fc7b3cd8f42713403dfe8114d7ba2b52907a178
jjin70 <jjin0434@naver.com>
2024-11-01 00:00:59 +0900

    BE: [feat] 유저 정보 추출 구현 #9

---
+++ src/backend/src/main/java/RunningMachines/R2R/domain/user/service/UserQueryService.java
@@ -0,0 +1,22 @@
+package RunningMachines.R2R.domain.user.service;
+
+import RunningMachines.R2R.domain.user.dto.UserResponseDto;
+import RunningMachines.R2R.domain.user.repository.UserRepository;
+import RunningMachines.R2R.global.exception.CustomException;
+import RunningMachines.R2R.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueryService {
+
+    private final UserRepository userRepository;
+
+    public UserResponseDto getUserInfo(String email) {
+        return userRepository.findByEmail(email)
+                .map(UserResponseDto::of)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+}
+
