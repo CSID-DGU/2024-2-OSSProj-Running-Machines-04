fc3c09f5cc24100020a6c4db93fc3f7b226a0f8d
jjin70 <jjin0434@naver.com>
2024-11-01 00:34:19 +0900

    BE: [refactor] Auth 관련 메소드 위치 변경 #9

--- src/backend/src/main/java/RunningMachines/R2R/domain/user/controller/AuthController.java
+++ src/backend/src/main/java/RunningMachines/R2R/domain/user/controller/AuthController.java
@@ -4,12 +4,17 @@ import RunningMachines.R2R.domain.user.dto.UserLoginRequestDto;
 import RunningMachines.R2R.domain.user.dto.UserResponseDto;
 import RunningMachines.R2R.domain.user.dto.UserSignupRequestDto;
 import RunningMachines.R2R.domain.user.service.AuthCommandService;
+import RunningMachines.R2R.domain.user.service.AuthQueryService;
+import RunningMachines.R2R.global.auth.CustomUserDetails;
 import RunningMachines.R2R.global.auth.TokenDto;
+import RunningMachines.R2R.global.auth.TokenProvider;
 import RunningMachines.R2R.global.auth.TokenRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +24,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthCommandService authCommandService;
+    private final AuthQueryService authQueryService;
+    private final TokenProvider tokenProvider;
 
     @Operation(summary = "회원가입")
     @PostMapping("/signup")
@@ -36,5 +43,22 @@ public class AuthController {
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
         return ResponseEntity.ok(authCommandService.reissue(tokenRequestDto));
+    }
+
+    // 헤더 토큰을 가져와서 getAuthentication으로 유저 정보 추출
+    @GetMapping("/user-info-1")
+    public ResponseEntity<UserResponseDto> getUserInfo(HttpServletRequest request) {
+        // 헤더에서 토큰 가져오기
+        String token = request.getHeader("Authorization").substring(7); // "Bearer " 이후의 토큰 값만 가져오기
+        // 토큰에서 이메일 정보 추출
+        String email = tokenProvider.getAuthentication(token).getName();
+        // 유저 정보 조회
+        return ResponseEntity.ok(authQueryService.getUserInfo(email));
+    }
+
+    // @AuthenticationPrincipal 통해 유저 정보 추출
+    @GetMapping("/user-info-2")
+    public ResponseEntity<UserResponseDto> getUserInfo1(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return ResponseEntity.ok(authQueryService.getUserInfo(customUserDetails.getUsername()));
     }
 }
--- src/backend/src/main/java/RunningMachines/R2R/domain/user/service/AuthQueryService.java
+++ src/backend/src/main/java/RunningMachines/R2R/domain/user/service/AuthQueryService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserQueryService {
+public class AuthQueryService {
 
     private final UserRepository userRepository;
 
