6dacca02fb74f4e5e90bd239402caeec7c1577c4
jjin70 <jjin0434@naver.com>
2024-11-07 03:10:46 +0900

    BE: [feat] 코스 정보 반환 구현 #17

---
+++ src/backend/src/main/java/RunningMachines/R2R/domain/course/dto/GpxResponseDto.java
@@ -0,0 +1,13 @@
+package RunningMachines.R2R.domain.course.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GpxResponseDto {
+    private String fileName;
+    private List<String> waypoints;
+}
