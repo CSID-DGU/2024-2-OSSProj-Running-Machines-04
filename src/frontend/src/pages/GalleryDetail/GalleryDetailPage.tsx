a91454ffdf31027bafe9027db262bf68e455feeb
Hyeona01 <sha0209@dgu.ac.kr>
2024-11-07 15:45:44 +0900

    FE: [feat] 크루 디테일 페이지 작업 #25

--- src/frontend/src/pages/GalleryDetail/GalleryDetailPage.tsx
+++ src/frontend/src/pages/GalleryDetail/GalleryDetailPage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { ReactComponent as BackIcon } from "@/assets/icons/BackIcon.svg";
+import ProfileCard from "@/components/common/ProfileCard";
+import GalleryContents from "@/components/galleryDetail/GalleryContents";
+import { useNavigate } from "react-router-dom";
 
 const GalleryDetailPage = () => {
-  return <div>GalleryDetailPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div className="shadow-sm w-full flex justify-start p-4">
+        <BackIcon onClick={() => navigate("/crew")} />
+      </div>
+      <ProfileCard />
+      <GalleryContents />
+    </div>
+  );
 };
 
 export default GalleryDetailPage;
