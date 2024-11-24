d9095876abbb12aecc0c6cd44b859ec4b9d6fd40
jjin70 <jjin0434@naver.com>
2024-11-24 15:12:39 +0900

    BE: [feat] 후기 기반 선호도 업데이트 #67

--- src/backend/src/main/java/RunningMachines/R2R/domain/course/service/ReviewCommandService.java
+++ src/backend/src/main/java/RunningMachines/R2R/domain/course/service/ReviewCommandService.java
@@ -6,8 +6,12 @@ import RunningMachines.R2R.domain.course.entity.*;
 import RunningMachines.R2R.domain.course.repository.ReviewRepository;
 import RunningMachines.R2R.domain.course.repository.TagRepository;
 import RunningMachines.R2R.domain.course.repository.UserCourseRepository;
+import RunningMachines.R2R.domain.user.entity.Elevation;
+import RunningMachines.R2R.domain.user.entity.Prefer;
 import RunningMachines.R2R.domain.user.entity.User;
+import RunningMachines.R2R.domain.user.repository.PreferRepository;
 import RunningMachines.R2R.domain.user.repository.UserRepository;
+import RunningMachines.R2R.domain.user.service.UserCommandService;
 import RunningMachines.R2R.global.exception.CustomException;
 import RunningMachines.R2R.global.exception.ErrorCode;
 import jakarta.transaction.Transactional;
@@ -23,6 +27,8 @@ public class ReviewCommandService {
     private final TagRepository tagRepository;
     private final UserRepository userRepository;
     private final UserCourseRepository userCourseRepository;
+    private final PreferRepository preferRepository;
+    private final UserCommandService userCommandService;
 
     public ReviewResponseDto createReview(String email, Long userCourseId, ReviewRequestDto reviewRequestDto) {
         User user = userRepository.findByEmail(email)
@@ -33,6 +39,13 @@ public class ReviewCommandService {
 
         Review review = reviewRequestDto.toEntity(user, userCourse);
         reviewRepository.save(review);
+
+        // 리뷰에서 받은 난이도 바탕으로 선호도 업데이트
+        Prefer prefer = preferRepository.findByUserId(user.getId())
+                .orElseThrow(() -> new CustomException(ErrorCode.PREFER_NOT_FOUND));
+
+        userCommandService.updatePreferElevation(prefer, reviewRequestDto.getDifficulty());
+        preferRepository.save(prefer);
 
         // 리뷰 태그 생성
         for (Long tagId : reviewRequestDto.getTagIds()) {
