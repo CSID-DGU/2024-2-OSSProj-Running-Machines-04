16c238b9189365a5ba74ca3a482535298612c18a
jjin70 <jjin0434@naver.com>
2024-11-24 19:43:45 +0900

    BE: [fix] S3 한글명 파일 업로드 에러 해결 #77

--- src/backend/src/main/java/RunningMachines/R2R/global/exception/ErrorCode.java
+++ src/backend/src/main/java/RunningMachines/R2R/global/exception/ErrorCode.java
@@ -22,16 +22,19 @@ public enum ErrorCode {
     INVALID_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "USER4003", "유효하지 않은 토큰입니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "USER4004", "유효하지 않은 토큰입니다."),
 
+    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE5000", "서버 에러: 파일 업로드에 실패했습니다."),
+    FILE_NAME_ENCODING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE5001", "서버 에러: 파일명 인코딩에 실패했습니다."),
+
     GPX_PARSING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "COURSE5000", "서버 에러: GPX 파싱에 실패했습니다."),
     GPX_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "COURSE5001", "서버 에러: GPX 업로드에 실패했습니다."),
 
-    COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "COURSE4001", "코스를 찾을 수 없습니다."),
+    COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "COURSE4000", "코스를 찾을 수 없습니다."),
 
-    USER_COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_COURSE4001", "사용자 코스를 찾을 수 없습니다."),
+    USER_COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_COURSE4000", "사용자 코스를 찾을 수 없습니다."),
 
-    TAG_NOT_FOUND(HttpStatus.NOT_FOUND, "TAG4001", "태그를 찾을 수 없습니다."),
+    TAG_NOT_FOUND(HttpStatus.NOT_FOUND, "TAG4000", "태그를 찾을 수 없습니다."),
 
-    PREFER_NOT_FOUND(HttpStatus.NOT_FOUND, "PREFER4001", "선호도가 입력되어 있지 않습니다.");
+    PREFER_NOT_FOUND(HttpStatus.NOT_FOUND, "PREFER4000", "선호도가 입력되어 있지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
