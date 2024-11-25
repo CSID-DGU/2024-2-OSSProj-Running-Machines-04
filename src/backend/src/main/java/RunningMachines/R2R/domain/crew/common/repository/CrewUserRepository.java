618f0d1ab3290bfb20d91d2b13168408f922183a
Seoyoung2222 <psy2877@dgu.ac.kr>
2024-11-25 23:42:15 +0900

    BE: [feat] 공지글 작성하기 #83

--- src/backend/src/main/java/RunningMachines/R2R/domain/crew/common/repository/CrewUserRepository.java
+++ src/backend/src/main/java/RunningMachines/R2R/domain/crew/common/repository/CrewUserRepository.java
@@ -1,7 +1,12 @@
 package RunningMachines.R2R.domain.crew.common.repository;
 
+import RunningMachines.R2R.domain.crew.common.entity.Crew;
 import RunningMachines.R2R.domain.crew.common.entity.CrewUser;
+import RunningMachines.R2R.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CrewUserRepository extends JpaRepository<CrewUser, Integer> {
+import java.util.Optional;
+
+public interface CrewUserRepository extends JpaRepository<CrewUser, Long> {
+    Optional<CrewUser> findByCrewAndUser(Crew crew, User user);
 }
