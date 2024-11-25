dfb89c586db974beb7c3f0e87845424425f4f537
Seoyoung2222 <psy2877@dgu.ac.kr>
2024-11-25 13:38:41 +0900

    BE: [feat] 댓글 삭제 기능 #36

--- src/backend/src/main/java/RunningMachines/R2R/domain/community/comment/repository/CommentRepository.java
+++ src/backend/src/main/java/RunningMachines/R2R/domain/community/comment/repository/CommentRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostIdAndParentCommentIsNull(Long postId);
+    List<Comment> findByParentCommentId(Long parentCommentId);
 }
