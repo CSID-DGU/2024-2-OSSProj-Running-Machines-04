4c87b482109cdbe7d4c7c75a57427e778c7aa3f5
jjin70 <jjin0434@naver.com>
2024-11-27 01:05:50 +0900

    BE: [feat] 리뷰 기반 top3 코스 태그 반환 #32

--- src/backend/src/main/java/RunningMachines/R2R/domain/course/controller/ReviewController.java
+++ src/backend/src/main/java/RunningMachines/R2R/domain/course/controller/ReviewController.java
@@ -5,6 +5,8 @@ import RunningMachines.R2R.domain.course.dto.ReviewResponseDto;
 import RunningMachines.R2R.domain.course.service.ReviewCommandService;
 import RunningMachines.R2R.global.auth.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +22,8 @@ public class ReviewController {
     private final ReviewCommandService reviewCommandService;
 
     @Operation(summary = "리뷰 작성",
-            description = "userCourseId는 러닝 기록 저장할 때 반환된 id <br> difficulty: LOW, MEDIUM, HIGH <br> tag는 산(1) 나무(2) 강(3) ... 연인(12) 반려동물(13)")
+            description = "difficulty: LOW, MEDIUM, HIGH <br> tag는 산(1) 나무(2) 강(3) ... 연인(12) 반려동물(13) <br> 러닝 기록과 동일하게 추천 코스 따라 뛰었다면 courseId에 해당 코스id 넣으면 됩니다")
+    @Parameter(name = "userCourseId", description = "러닝 기록 저장할 때 반환된 id")
     @PostMapping("/review/{userCourseId}")
     public ResponseEntity<ReviewResponseDto> createReview(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable Long userCourseId, @RequestBody ReviewRequestDto reviewRequestDto) {
         return ResponseEntity.ok(reviewCommandService.createReview(customUserDetails.getUsername(), userCourseId, reviewRequestDto));
