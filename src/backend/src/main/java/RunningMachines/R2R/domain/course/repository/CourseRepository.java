565c614a2eb30c2dac5187b4b91669499a02aed2
jjin70 <jjin0434@naver.com>
2024-12-01 18:47:16 +0900

    BE: [feat] 코스 추천 파이썬 모델 연동 #111

--- src/backend/src/main/java/RunningMachines/R2R/domain/course/repository/CourseRepository.java
+++ src/backend/src/main/java/RunningMachines/R2R/domain/course/repository/CourseRepository.java
@@ -2,18 +2,7 @@ package RunningMachines.R2R.domain.course.repository;
 
 import RunningMachines.R2R.domain.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-//    @Query("SELECT c.distance FROM Course c WHERE c.fileName = :fileName")
-//    double findDistanceByFileName(@Param("fileName") String fileName);
-
     Course findByFileName(String fileName);
-
-    @Query("SELECT c.courseUrl FROM Course c")
-    List<String> findAllCourseUrls();
 }
