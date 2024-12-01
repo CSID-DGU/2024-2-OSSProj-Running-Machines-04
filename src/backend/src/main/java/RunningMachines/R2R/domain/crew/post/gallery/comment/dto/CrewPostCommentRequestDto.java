0b268b22c34c3637a308dc5fe602190445ebdc6f
Seoyoung2222 <psy2877@dgu.ac.kr>
2024-12-01 20:58:21 +0900

    BE: [feat] 갤러리 댓글 달기 #83

---
+++ src/backend/src/main/java/RunningMachines/R2R/domain/crew/post/gallery/comment/dto/CrewPostCommentRequestDto.java
@@ -0,0 +1,12 @@
+package RunningMachines.R2R.domain.crew.post.gallery.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CrewPostCommentRequestDto {
+    private String content;
+}
