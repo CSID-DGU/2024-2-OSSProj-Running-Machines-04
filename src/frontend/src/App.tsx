4d7d34d1c8c720aca50a5533f8cba5540d2bbfd5
Hyeona01 <sha0209@dgu.ac.kr>
2024-12-04 16:30:41 +0900

    FE: [feat] 회원가입 이동 추가 및 로그인 유저 검증

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,16 +1,29 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ScrollToTop from "@components/common/ScrollToTop";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "@components/common/Header";
 import Navbar from "@components/common/Navbar";
+import { useEffect } from "react";
+import { getAccessToken } from "./utils/auth";
 
 const queryClient = new QueryClient();
 
 const Layout = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   const noHeaderPath = ["/record", "/community/write"];
   const noNavbarPath = ["/other-crew", "/signin", "/signup"];
+  const authPath = ["/", "/signin", "/signup"];
+
+  useEffect(() => {
+    const accessToken = getAccessToken();
+    // '/' 경로가 아니고, accessToken이 없을 때 '/signin'으로 이동
+    if (!accessToken && !authPath.includes(pathname)) {
+      alert("로그인이 필요합니다!");
+      navigate("/signin", { replace: true });
+    }
+  }, [pathname, navigate]);
 
   return (
     <div className="w-screen min-h-screen bg-white flex justify-center items-center">
