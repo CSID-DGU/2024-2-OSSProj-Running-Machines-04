270335199fab45cc2014086d879224abad26825a
jjin70 <jjin0434@naver.com>
2024-12-05 21:40:39 +0900

    BE: [fix] swagger cors 설정 수정

--- src/backend/src/main/java/RunningMachines/R2R/global/config/SwaggerConfig.java
+++ src/backend/src/main/java/RunningMachines/R2R/global/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package RunningMachines.R2R.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -9,6 +11,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "로컬 서버 URL"
+                ),
+                @Server(
+                        url = "https://52.78.82.12.nip.io",
+                        description = "HTTPS 서버 URL"
+                )
+        }
+)
 public class SwaggerConfig {
 
     private static final String SCHEME_NAME = "Authorization";
