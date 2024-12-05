373646a4ee48559a6d6ca71ede30bc9985a48b82
jjin70 <jjin0434@naver.com>
2024-12-05 13:19:37 +0900

    BE: [feat] 추천 코스 반환값에 편의시설 위경도 리스트 추가 #111

--- src/backend/src/main/java/RunningMachines/R2R/domain/course/dto/CourseResponseDto.java
+++ src/backend/src/main/java/RunningMachines/R2R/domain/course/dto/CourseResponseDto.java
@@ -1,10 +1,14 @@
 package RunningMachines.R2R.domain.course.dto;
 
 import RunningMachines.R2R.domain.course.entity.Course;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @Builder
@@ -19,7 +23,9 @@ public class CourseResponseDto {
     private double distance;
     private List<String> tags;
     private Long toiletCounts;
+    private List<CoordinateDto> toiletLocation;
     private Long storeCounts;
+    private List<CoordinateDto> storeLocation;
     private Long trafficLightCounts;
     private boolean courseLiked;
 
@@ -33,9 +39,22 @@ public class CourseResponseDto {
                 .distance(course.getDistance())
                 .tags(tags)
                 .toiletCounts(course.getToiletCounts())
+                .toiletLocation(parseCoordinates(course.getToiletLocation()))
                 .storeCounts(course.getStoreCounts())
+                .storeLocation(parseCoordinates(course.getStoreLocation()))
                 .trafficLightCounts(course.getTrafficLightCounts())
                 .courseLiked(courseLiked)
                 .build();
     }
+
+    private static List<CoordinateDto> parseCoordinates(String json) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(json, new TypeReference<List<CoordinateDto>>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
 }
+
