02ef9d5b05fc872d201ac43e80338bb9f307ed24
Hyeona01 <sha0209@dgu.ac.kr>
2024-12-10 16:10:21 +0900

    FE: [feat] 러닝기록 초에서 분단위 변경

--- src/frontend/src/pages/Mypage/MyPage.jsx
+++ src/frontend/src/pages/Mypage/MyPage.jsx
@@ -4,8 +4,10 @@ import Profile from "@/components/mypage/Profile";
 import RecentRecord from "@/components/mypage/RecentRecord";
 import Stats from "@/components/mypage/Stats";
 import { useCalendarGet, useRecentStatsGet } from "@/hooks/useMypage";
+import { useNavigate } from "react-router-dom";
 
 const MyPage = () => {
+  const navigate = useNavigate();
   const { data, isLoading } = useRecentStatsGet();
   const { data: calendar } = useCalendarGet(2024, 12);
 
@@ -15,7 +17,7 @@ const MyPage = () => {
         <Spinner />
       ) : (
         data && (
-          <div className="mb-32">
+          <div className="mb-20">
             <Profile />
             <Calendar runningData={calendar} />
             <h1 className="text-[24px] font-semibold px-10 mt-12">최근 기록</h1>
@@ -23,6 +25,12 @@ const MyPage = () => {
               <RecentRecord record={record} />
             ))}
             <Stats />
+            <p
+              onClick={() => navigate("/signin")}
+              className="text-end underline mx-10 mt-10 text-gray-400"
+            >
+              로그인
+            </p>
           </div>
         )
       )}
